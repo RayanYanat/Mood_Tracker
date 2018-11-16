be40cdd63dff5b993da80aa3260fe0f1cff539da
Rayan <rayan.yanat@gmail.com>
2018-11-16 18:30:04 +0100

    creation of a popup with edit text when the user presses the comment button

--- app/src/main/java/com/example/rayan/mood_tracker/MainActivity.java
+++ app/src/main/java/com/example/rayan/mood_tracker/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.rayan.mood_tracker;
 
 import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.rayan.mood_tracker.adapters.RecyclerAdapter;
 
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button historyButton;
     private Button commentButton;
+    private EditText commentPopup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.MyRecyclerView);
         historyButton = findViewById(R.id.activity_main_history_btn);
         commentButton = findViewById(R.id.activity_main_comment_btn);
+        commentPopup = findViewById(R.id.comment_Popup);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
@@ -40,5 +46,19 @@ public class MainActivity extends AppCompatActivity {
         snapHelper.attachToRecyclerView(recyclerView);
         adapter = new RecyclerAdapter(images,colors);
         recyclerView.setAdapter(adapter);
+
+        commentButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("commentary");
+                        View mView = LayoutInflater.from(MainActivity.this).inflate(R.layout.comment_popup,null);
+                builder.setView(mView);
+                        commentPopup = (EditText)mView.findViewById(R.id.comment_Popup);
+                        commentPopup.getText().toString();
+                builder.show();
+
+            }
+            });
     }
 }
